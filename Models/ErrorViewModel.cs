7ad431541e1d01d967521563c3cccf265776274a
saritalad <sarita.lad@rediffmail.com>
2022-12-02 05:56:46 +0000

    MVC simple App pushed for docker

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCAppSimple.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
